0c40803f51869df0c3203b3b0f8fdf28f69db2c1
Vincent Barreau <guyzmuch@gmail.com>
2019-08-03 12:24:39 +0200

    Create pendu.js

---
+++ javascript/level_2/pendu.js
@@ -0,0 +1,15 @@
+/*
+  CREER UN JEU DE PENDU
+*/
+
+// ///////////////////
+// INSTRUCTION :
+
+// L'aplication choisi un mot parmi un liste
+// on affiche un promt pour que l'utilisateur rentre une lettre
+// On affiche sur le mot la lettre la ou elle ce trouve
+// Exemple 1: "M _ _ _ _ _ _"
+// Exemple 2: "M _ _ _ R _ R"
+// Si le joueur trouve toutes les lettres du mot, il gagne
+// si après 5 fois où le joueur n'a pas trouver de lettre du mot il a perdu
+
