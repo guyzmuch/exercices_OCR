3e6e15bd66cef8da042b8a166b6fd7612903bd88
Vincent Barreau <guyzmuch@gmail.com>
2019-08-03 12:21:15 +0200

    Create deviner_le_nombre.js

---
+++ javascript/level_2/deviner_le_nombre.js
@@ -0,0 +1,18 @@
+/*
+  JEU OU L'UTILISATEUR DOIT DEVINER LE NOMBRE CHOISI AU HASARD,
+  EN 10 ESSAIS
+*/
+
+// ///////////////////
+// INSTRUCTION :
+
+// L'aplication choisi une nombre entre 1 et 100
+// on affiche un promt pour que l'utilisateur rentre un chiffre
+// Suivant le chiffre, on dis si le nombre est supérieur ou inférieur
+// Si le joueur trouve le bon chiffre, il gagne
+// si après 10 essais il n'a pas trouver, il a perdu
+
+
+// ///////////////////
+// L'exercice est prit de ici
+//https://developer.mozilla.org/fr/docs/Learn/JavaScript/First_steps/A_first_splash
