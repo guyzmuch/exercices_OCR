33da42f61da7dc7c5dee873edd5153941bfd248c
Vincent Barreau <guyzmuch@gmail.com>
2019-08-08 13:29:57 +0200

    Create temp.js

---
+++ javascript/basic/temp.js
@@ -0,0 +1,24 @@
+// Exercice 1:
+// Une fonction qui demande à l'utilisateur sont nom, puis affiche "bonjour" avec le prénom en console
+
+
+// Exercice 2:
+// Une fonction qui demande un mot de passe. Si il est bon on affiche "entrez" si il est mauvais on affiche "incorect"
+// (le mot de passe peut être un mot quelconque a choisir)
+
+
+
+// Exercice 3:
+// fonction qui calcule le périmettre d'un parallélogramme. Formule : longueur plus largeur, le tout fois 2
+
+
+
+
+
+
+// exercice 20:
+// Fonction qui dis quel sera le jour après celui passé en paramêtre
+// example 1: si je passe "lundi" on me renvoie "mardi"
+// example 2: si je passe "vendredi" on me renvoie "samedi"
+// Si joint le tableau des jours a utiliser
+let weekDay = ["lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"]
