be5633ac38f6b9ac7dcd686e5211d5026cce1616
Vincent Barreau <guyzmuch@gmail.com>
2019-08-09 17:28:48 +0200

    Update temp.js

--- javascript/basic/temp.js
+++ javascript/basic/temp.js
@@ -12,6 +12,27 @@
 // fonction qui calcule le périmettre d'un parallélogramme. Formule : longueur plus largeur, le tout fois 2
 
 
+// Exercice 4:
+// fonction affiche si la lettre passé est une consonne ou une voyelle
+
+// Exercice 5:
+// fonction affiche pour chaque lettre de l'alphabet, si c'est un consonne ou une voyelle
+// example : "a: voyelle"
+let alphabet = [
+  "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+  "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+]
+
+
+
+
+
+
+// Exercice 10:
+// Créer une fonction qui écrit tout les chiffres pair jusqu'à 20
+
+
+
 
 
 
@@ -22,3 +43,5 @@
 // example 2: si je passe "vendredi" on me renvoie "samedi"
 // Si joint le tableau des jours a utiliser
 let weekDay = ["lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"]
+
+let today = "lundi"
