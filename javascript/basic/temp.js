38bce67de8195ca8f5a026805cc8bc68cd4573eb
Vincent Barreau <guyzmuch@gmail.com>
2019-08-09 17:36:38 +0200

    Update temp.js

--- javascript/basic/temp.js
+++ javascript/basic/temp.js
@@ -15,14 +15,30 @@
 // Exercice 4:
 // fonction affiche si la lettre passé est une consonne ou une voyelle
 
-// Exercice 5:
-// fonction affiche pour chaque lettre de l'alphabet, si c'est un consonne ou une voyelle
-// example : "a: voyelle"
-let alphabet = [
-  "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-  "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
-]
 
+
+// exercice 6:
+// Créer une fonction qui retourne la valeur absolue (toujours positif) d'un nombre (sans utiliser la library "Math)
+// example 1 : valeurAbsoluts(4) retourne 4
+// example 2 : valeurAbsoluts(-81) retourne 81
+
+
+
+// Exercice 7:
+// Créer une fonction qui retourne le chiffre le plus grand parmis les 2 passé en paramettre (sans utiliser Math.max)
+// Il retourn le texte "égaux" si il sont pareil
+// example 1 : max(4, 8000) retourne 8000
+// example 2 : max(81, 80) retourne 81
+// example 1 : max(4, 4) retourne "égaux"
+
+
+// Exercice 8
+// Créer la fonction calculer() qui gère les 4 opérations mathématiques de base : 
+// addition, soustraction, multiplication et division.
+console.log(calculer(4, "+", 6));  // 10
+console.log(calculer(4, "-", 6));  // -2
+console.log(calculer(2, "*", 0));  // 0
+console.log(calculer(12, "/", 0)); // Infinity
 
 
 
@@ -34,6 +50,13 @@ let alphabet = [
 
 
 
+// Exercice 11:
+// fonction affiche pour chaque lettre de l'alphabet, si c'est un consonne ou une voyelle
+// example : "a: voyelle"
+let alphabet = [
+  "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+  "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+]
 
 
 
